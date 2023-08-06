02e9798256d35c6fc40aee1ee2b23714e8b18f79
Peter Aarestad <aarestad@gmail.com>
2023-08-06 12:56:43 -0500

    refactor Sphere

--- src/objects/sphere.rs
+++ src/objects/sphere.rs
@@ -32,32 +32,10 @@ impl Sphere {
 }
 
 impl PrivateObject for Sphere {
-    fn local_intersect(&self, _local_ray: &Ray) -> Intersections {
-        unimplemented!()
-    }
-
-    fn local_normal_at(&self, _local_point: &Point) -> Vector {
-        unimplemented!()
-    }
-}
-
-impl Object for Sphere {
-    fn as_sphere(&self) -> &Sphere {
-        self
-    }
-
-    fn intersections(&self, ray: &Ray) -> Intersections {
-        let transformed_ray = ray.transform(
-            &self
-                .0
-                .transform
-                .try_inverse()
-                .expect("cannot invert transform"),
-        );
-
-        let sphere_to_ray = transformed_ray.origin - Point::origin();
-        let a = transformed_ray.direction.dot(&transformed_ray.direction);
-        let b = 2. * transformed_ray.direction.dot(&sphere_to_ray);
+    fn local_intersect(&self, local_ray: &Ray) -> Intersections {
+        let sphere_to_ray = local_ray.origin - Point::origin();
+        let a = local_ray.direction.dot(&local_ray.direction);
+        let b = 2. * local_ray.direction.dot(&sphere_to_ray);
         let c = sphere_to_ray.dot(&sphere_to_ray) - 1.;
 
         let discriminant = b.powi(2) - 4. * a * c;
@@ -72,24 +50,21 @@ impl Object for Sphere {
         ])
     }
 
+    fn local_normal_at(&self, local_point: &Point) -> Vector {
+        *local_point - Point::origin()
+    }
+}
+
+impl Object for Sphere {
+    fn as_sphere(&self) -> &Sphere {
+        self
+    }
+
     fn transform(&self) -> &Transform {
         &self.0.transform
     }
 
     fn material(&self) -> &Material {
         &self.0.material
-    }
-
-    fn normal_at(&self, p: Point) -> Vector {
-        let t = self
-            .0
-            .transform
-            .try_inverse()
-            .expect("transform not invertible");
-
-        let object_point = p.transform(&t);
-        let object_normal = object_point - Point::origin();
-        let world_normal = object_normal.transform(&t.transpose());
-        world_normal.normalize()
     }
 }
