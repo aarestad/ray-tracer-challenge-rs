42c5c92d1f0c54c39598afd0a463469de4be1f5d
Peter Aarestad <aarestad@gmail.com>
2023-08-12 12:00:34 -0500

    add args to all main

--- tests/all.rs
+++ tests/all.rs
@@ -1,4 +1,5 @@
-use cucumber::World;
+use clap::arg;
+use cucumber::{cli, World};
 use futures_lite::future;
 use std::env::args;
 use testutils::world::RayTracerWorld;
@@ -13,18 +14,35 @@ const ALL_TESTS: [&str; 7] = [
     "patterns",
 ];
 
+#[derive(cli::Args, Clone)]
+struct CustomOpts {
+    #[arg(long)]
+    rt_tests: Option<String>,
+}
+
 fn main() {
     let mut tests: Vec<String> = args().collect();
+    dbg!(&tests);
 
-    if tests.is_empty() {
+    let opts = cli::Opts::<_, _, _, CustomOpts>::parsed();
+    let tests_opt = opts.custom.rt_tests.clone();
+
+    if let Some(tests_str) = tests_opt {
+        tests.extend(
+            tests_str
+                .split(',')
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>(),
+        );
+    } else {
         tests.extend(ALL_TESTS.iter().map(|s| s.to_string()));
     }
 
-    // future::block_on(RayTracerWorld::run("tests/features/lights.feature"));
-    // future::block_on(RayTracerWorld::run("tests/features/materials.feature"));
-    // future::block_on(RayTracerWorld::run("tests/features/world.feature"));
-    // future::block_on(RayTracerWorld::run("tests/features/camera.feature"));
-    // future::block_on(RayTracerWorld::run("tests/features/shapes.feature"));
-    // future::block_on(RayTracerWorld::run("tests/features/planes.feature"));
-    future::block_on(RayTracerWorld::run("tests/features/patterns.feature"));
+    for t in tests {
+        future::block_on(
+            RayTracerWorld::cucumber()
+                .with_cli(opts.clone())
+                .run(format!("tests/features/{}.feature", t)),
+        );
+    }
 }
